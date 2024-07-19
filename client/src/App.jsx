d95ba77e550ac03dd7ce3f8dcb23e694e9726f1c
Marcel Spisak <marcel.jacek.spisak@gmail.com>
2024-07-19 15:24:16 +0200

    Add styled components, remove css files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,41 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 import RecipeForm from './components/RecipeForm/RecipeForm';
 import RecipePreview from './components/RecipePreview/RecipePreview';
 
-function App() {
+const AppWrapper = styled.div`
+  font-family: 'Arial', sans-serif;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f0f0f0;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex-column;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 20px;
+  color: #333;
+`;
+
+const ErrorMessage = styled.p`
+  background-color: #f8d7da;
+  color: #721c24;
+  padding: 10px;
+  border: 1px solid #f5c6cb;
+  border-radius: 5px;
+  margin-bottom: 20px;
+`;
+
+const App = () => {
   const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchRecipes = async (ingredients) => {
+    setIsLoading(true);
     try {
       const response = await axios.post('http://localhost:5001/api/recipes', { ingredients });
       var responseContent = response.data.choices[0].message.content.trim();
@@ -15,16 +44,23 @@ function App() {
       setRecipes(recipe);
     } catch (error) {
       console.error('Error fetching recipes:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="App">
-      <h1>AI-Powered Recipe Generator</h1>
+    <AppWrapper>
+      <Title>AI-Powered Recipe Generator</Title>
       <RecipeForm onIngredientsSubmit={fetchRecipes} />
-      <RecipePreview recipe={recipes} />
-    </div>
+      <RecipePreview isLoading={isLoading} recipe={recipes} />
+    </AppWrapper>
+    // <div className="App">
+    //   <h1>AI-Powered Recipe Generator</h1>
+    //   <RecipeForm onIngredientsSubmit={fetchRecipes} />
+    //   <RecipePreview recipe={recipes} />
+    // </div>
   );
-}
+};
 
 export default App;
--- client/src/components/RecipePreview/RecipePreview.jsx
+++ client/src/components/RecipePreview/RecipePreview.jsx
@@ -1,15 +1,60 @@
 import React from 'react';
-import './RecipePreview.css'; // Import the CSS file for styling
+import styled from 'styled-components';
 
-const RecipePreview = ({ recipe }) => {
-  if (recipe.length === 0) {
-    return <div>No recipe to display</div>;
+const RecipePreviewContainer = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 16px;
+  margin: 16px 32px 16px 32px;
+  margin-top: 16px;
+  background-color: #f9f9f9;
+`;
+
+const RecipeDetails = styled.div`
+  margin-bottom: 16px;
+
+  div {
+    margin-bottom: 4px;
+  }
+`;
+
+const RecipeSection = styled.div`
+  margin-bottom: 16px;
+
+  h3 {
+    margin-bottom: 8px;
+    font-weight: bold;
+  }
+
+  ul {
+    padding-left: 20px;
+  }
+
+  li {
+    margin-bottom: 4px;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 24px;
+  margin-bottom: 8px;
+`;
+
+const Label = styled.div`
+  text-align: center;
+`;
+
+const RecipePreview = ({ isLoading, recipe }) => {
+  if (!isLoading && recipe.length === 0) {
+    return <Label>No recipe to display</Label>;
+  } else if (isLoading) {
+    return <Label>We are generating your recipe...</Label>
   }
 
   return (
-    <div className="recipe-preview">
-      <h2 className="recipe-title">{recipe.title}</h2>
-      <div className="recipe-details">
+    <RecipePreviewContainer>
+      <Title>{recipe.title}</Title>
+      <RecipeDetails>
         <div>
           <strong>Prep Time:</strong> {recipe.prepTime}
         </div>
@@ -19,24 +64,24 @@ const RecipePreview = ({ recipe }) => {
         <div>
           <strong>Difficulty:</strong> {recipe.difficulty}
         </div>
-      </div>
-      <div className="recipe-section">
+      </RecipeDetails>
+      <RecipeSection>
         <h3>Ingredients</h3>
         <ul>
           {recipe.ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient}</li>
           ))}
         </ul>
-      </div>
-      <div className="recipe-section">
+      </RecipeSection>
+      <RecipeSection>
         <h3>Instructions</h3>
         <ul>
           {recipe.instructions.map((instruction, index) => (
             <li key={index}>{instruction}</li>
           ))}
         </ul>
-      </div>
-    </div>
+      </RecipeSection>
+    </RecipePreviewContainer>
   );
 };
 
